32f8712dd176614278ec8a056b2fac7c2eeeb08f
Mathieu Gautier <mathieu@alqemist.com>
2023-10-13 12:01:51 +0200

    DRY main.rb files

--- backend/utils/data.rb
+++ backend/utils/data.rb
@@ -1,6 +1,22 @@
 require 'json'
+require_relative '../models/car'
 
 module GetAround
+
+  def self.compute_rental_fares(rental_clazz)
+    data_json = GetAround.file_to_json('input.json')
+
+    cars = data_json[:cars].map { |car_attrs|
+      Car.new(*car_attrs.values_at(*Car.members))
+    }
+
+    _rentals = data_json[:rentals].map { |rental_attrs|
+      rental     = rental_clazz.new(*rental_attrs.values_at(*rental_clazz.members))
+      rental.car = cars.select { |c| c.id == rental.car_id }.first
+      rental.output
+    }
+  end
+
   def self.file_to_json(filename)
     begin
       file = File.read("data/#{filename}")
